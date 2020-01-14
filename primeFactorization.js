464c3b7969e4d26af28f00c340321c465440d2e2
かずま <51999502+kazumax7@users.noreply.github.com>
2020-01-15 03:42:04 +0900

    more faster!!!

--- primeFactorization.js
+++ primeFactorization.js
@@ -2,22 +2,20 @@ function primeFactorization(target){
 	if(isNaN(target)) return;
 	
 	target = Math.floor(target);
-	let result;
-	
+
 	switch(target){
 		case 0:
 		case 1:
-			result = [target];
-			break;
-		
+			return [target];
+
 		default:
-			result = [];
+			let result = [];
 			
 			if(target < 0){
 				target *= -1;
 				result = [-1];
 			}
-			for(let i = 2; i <= target; i++){
+			for(let i = 2; i <= Math.sqrt(target);  i++){
 				if(i % 2 == 0 && i != 2) continue;
 				if(target % i == 0){
 					result = result.concat([i]);
@@ -25,10 +23,9 @@ function primeFactorization(target){
 					let addition = primeFactorization(target/i);
 					if(addition[0] != 1) result = result.concat(addition);
 					
-					break;
+					return result;
 				}
 			}
+			return result.concat([target]);
 	}
-	
-	return result;
 }
